faf79f6981628878877f007c0efb58314f42ba86
JackGerrity <jgerrity@cs.ua.edu>
2014-05-06 10:46:08 -0500

    Initial Load

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Jack Gerrity on 5/5/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
